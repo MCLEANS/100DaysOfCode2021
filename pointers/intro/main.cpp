70910bf7e1a60973d11c42a07444ca623eb6dbdf
MCLEANS <jackagwaya@gmail.com>
2021-09-23 16:35:03 +0300

    accessing value by dereferencing pointer

--- pointers/intro/main.cpp
+++ pointers/intro/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+/**
+ * 1. Create a pointer to an integer
+ * 2. Print out the address of the pointer
+ * 3. Print out the value stored in the pointer address using the pointer
+ */
+
 int main(){
-	std::cout<<"Hello World!"<<std::endl;	
+	int my_interger = 23;
+	int* pointer_to_my_interger = &my_interger;
+	std::cout<<"The address of the pointer is "<<pointer_to_my_interger<<std::endl;
+	std::cout<<"The value stored in the pointer address is "<<*pointer_to_my_interger<<std::endl;
 	return 0;
 }
