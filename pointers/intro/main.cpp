6f5979db7a7ad1cf03a0b3e290cbcbefaf502d6b
MCLEANS <jackagwaya@gmail.com>
2021-09-23 21:28:40 +0300

    added passing pointer as function parameter

--- pointers/intro/main.cpp
+++ pointers/intro/main.cpp
@@ -6,10 +6,20 @@
  * 3. Print out the value stored in the pointer address using the pointer
  */
 
+void access_value(double *value){
+	*value = 233.90;
+}
+
 int main(){
 	int my_interger = 23;
 	int* pointer_to_my_interger = &my_interger;
 	std::cout<<"The address of the pointer is "<<pointer_to_my_interger<<std::endl;
 	std::cout<<"The value stored in the pointer address is "<<*pointer_to_my_interger<<std::endl;
+
+	std::cout<<"----------------------------------------------------------"<<std::endl;
+	double my_value = 56.90;
+	std::cout<<"1. My value is "<<my_value<<std::endl;
+	access_value(&my_value);
+	std::cout<<"2. My value is "<<my_value<<std::endl;
 	return 0;
 }
