789a62cf058f3cbebd2f12884feb64046c0d0404
MCLEANS <jackagwaya@gmail.com>
2021-10-04 22:45:09 +0300

    declared animal class

---
+++ pointers/new_operator/animal.h
@@ -0,0 +1,17 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H 
+
+class Animal{
+    private:
+        std::string name;
+    private:
+    public:
+    public:
+        Animal();
+        Animal(std::string name);
+        void set_name(std::string name);
+        void speak();
+};
+
+
+#endif //ANIMAL_H
